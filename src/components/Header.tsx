abb85bfdb11533aeeae6d1452b67517a985bdc3c
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-03-31 23:46:09 -0300

    chore(*): Split App into components

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export function Header({ selectedGenre }) {
+  return (
+    <header>
+      <span className="category">
+        Categoria:<span> {selectedGenre.title}</span>
+      </span>
+    </header>
+  );
+}
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,18 @@
-export function SideBar() {
-  // Complete aqui
+import "../styles/sidebar.scss";
+import { Button } from "./Button";
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }) {
+  return (
+    <div className="buttons-container">
+      {genres.map((genre) => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  );
 }
